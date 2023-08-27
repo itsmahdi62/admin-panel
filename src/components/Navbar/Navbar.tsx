218e180b196e7934ba24aa57bbbe13c134c8fd22
Mahdi <amiralmasi021@gmail.com>
2023-08-28 00:19:36 +0330

    Write html section of navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,7 +1,29 @@
 import "./Navbar.scss"
 
 const Navbar = () => {
-    return ( <div>Navbar</div> );
+    return ( <div className="navbar">
+        <div  className="logo">
+            <img src="logo.svg" alt="" />
+            <span>lamadmin</span>
+        </div>
+        <div className="icon">
+        <img src="/search.svg" alt="" />
+        <img src="/app.svg" alt="" />
+        <img src="/expand.svg" alt="" />
+        <div>
+            <img alt="" />
+        </div>
+        <div className="notifiction">
+            <img src="/notifiction.svg" alt="" />
+            <span>1</span>
+        </div>
+        <div className="user">
+            <img src="/settings.svg" alt="" />
+            <span>Mahdi</span>
+        </div>
+        <img src="/settings.svg" alt="" />
+        </div>    
+    </div> );
 }
  
 export default Navbar;
