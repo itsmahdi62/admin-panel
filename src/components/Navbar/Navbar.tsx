dde89575fb3ae632ce33793395891120134ffde2
Mahdi <amiralmasi021@gmail.com>
2023-08-28 12:20:24 +0330

    Create navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,27 +1,26 @@
 import "./Navbar.scss"
 
+import {AiOutlineAppstore, AiOutlineExpand, AiOutlineSearch, AiOutlineSetting} from "react-icons/ai"
+
 const Navbar = () => {
     return ( <div className="navbar">
         <div  className="logo">
             <img src="logo.svg" alt="" />
             <span>lamadmin</span>
         </div>
-        <div className="icon">
-        <img src="/search.svg" alt="" />
-        <img src="/app.svg" alt="" />
-        <img src="/expand.svg" alt="" />
-        <div>
-            <img alt="" />
-        </div>
-        <div className="notifiction">
-            <img src="/notifiction.svg" alt="" />
-            <span>1</span>
-        </div>
-        <div className="user">
-            <img src="/settings.svg" alt="" />
-            <span>Mahdi</span>
-        </div>
-        <img src="/settings.svg" alt="" />
+        <div className="icons">
+            <AiOutlineSearch />
+            <AiOutlineAppstore />
+            <AiOutlineExpand/>
+            <div className="notifications">
+                <img src="/notifications.svg" alt="" />
+                <span>1</span>
+            </div>
+            <div className="user">
+                <img src="https://avatars.githubusercontent.com/u/95579094?v=4" alt="" />
+                <span>Mahdi</span>
+            </div>
+            <AiOutlineSetting />
         </div>    
     </div> );
 }
