a549e7fb4675bdc980812de7e197861c925b3280
Mahdi <amiralmasi021@gmail.com>
2023-08-29 21:39:35 +0330

    Create charts

--- src/Data.ts
+++ src/Data.ts
@@ -124,3 +124,74 @@ export const topDealers =[
         amount:3.555,
     },
 ]
+
+export const chartBoxUser = {
+    color: "#8884d8",
+    icon:"/userIcon.svg",
+    title:"Total Users",
+    number: "11.238",
+    dataKey: "users",
+    percentage:45,
+    chartData: [
+        { name : "Sun" , users:400},
+        { name : "Mon" , users:600},
+        { name : "Tue" , users:500},
+        { name : "Wed" , users:700},
+        { name : "Thu" , users:400},
+        { name : "Fri" , users:500},
+        { name : "Sat" , users:450},     
+    ]
+}
+
+export const chartBoxConversion = {
+    color: "gold",     
+    icon:"/conversionIcon.svg",     
+    title:"Total conversion",     
+    number: "2.6",     
+    dataKey: "conversion",     
+    percentage:15,     
+    chartData: [     
+        { name : "Sun" , conversion:400},     
+        { name : "Mon" , conversion:600},     
+        { name : "Tue" , conversion:500},     
+        { name : "Wed" , conversion:700},     
+        { name : "Thu" , conversion:400},     
+        { name : "Fri" , conversion:500},     
+        { name : "Sat" , conversion:450},     
+    ]     
+}
+
+export const chartBoxRevenue = {
+    color: "teal",
+    icon:"/revenueIcon.svg",
+    title:"Total Revenue",
+    number: "$58.555",
+    dataKey: "revenue",
+    percentage:-15,
+    chartData: [
+        { name : "Sun" , revenue:90},
+        { name : "Mon" , revenue:600},
+        { name : "Tue" , revenue:500},
+        { name : "Wed" , revenue:70},
+        { name : "Thu" , revenue:460},
+        { name : "Fri" , revenue:500},
+        { name : "Sat" , revenue:450},
+    ]
+}
+export const chartBoxProduct = {
+    color: "gold",
+    icon:"/conversionIcon.svg",
+    title:"Total Ratio",
+    number: 45,
+    dataKey: "product",
+    percentage:-25,
+    chartData: [
+        { name : "Sun" , product:400},
+        { name : "Mon" , product:600},
+        { name : "Tue" , product:500},
+        { name : "Wed" , product:40},
+        { name : "Thu" , product:800},
+        { name : "Fri" , product:550},
+        { name : "Sat" , product:450},
+    ]
+}
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,3 +1,5 @@
+import { chartBoxUser ,chartBoxProduct  ,chartBoxRevenue ,chartBoxConversion } from "../../Data";
+import ChartBox from "../../components/ChartBox/ChartBox";
 import TopBox from "../../components/TopBox/TopBox";
 import "./Home.scss"
 
@@ -6,11 +8,11 @@ const Home = () => {
         <div className="box box1">
             <TopBox />
         </div>
-        <div className="box box2">Box2</div>
-        <div className="box box3">Box3</div>
-        <div className="box box4">Box4</div>
-        <div className="box box5">Box5</div>
-        <div className="box box6">Box6</div>
+        <div className="box box2"><ChartBox {...chartBoxUser}/></div>
+        <div className="box box3"><ChartBox {...chartBoxProduct}/></div>
+        <div className="box box4"></div>
+        <div className="box box5"><ChartBox {...chartBoxRevenue}/></div>
+        <div className="box box6"><ChartBox {...chartBoxConversion}/></div>
         <div className="box box7">Box7</div>
         <div className="box box8">Box8</div>
         <div className="box box9">Box9</div>
